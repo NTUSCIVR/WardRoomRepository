c804199447a5d3b890865b2dd9d733979148ed41
buttception <spartandevilexdreme@gmail.com>
2018-12-04 18:05:07 +0800

    Able to "eat " objects

---
+++ WardRoomProject/Assets/Scripts/InteractionScripts/Utensils.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Utensils : MonoBehaviour {
+    [SerializeField]
+    VRTK_InteractableObject linkedObject;
+    [SerializeField]
+    GameObject objectToDisable;
+    [SerializeField]
+    BoxCollider collider;
+    protected virtual void OnEnable()
+    {
+        linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+        collider = (collider == null ? GetComponent<BoxCollider>() : collider);
+
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+            linkedObject.InteractableObjectGrabbed += InteractableObjectGrabbed;
+            linkedObject.InteractableObjectSnappedToDropZone += InteractableObjectSnappedToDropZone;
+        }
+
+    }
+
+    protected virtual void OnDisable()
+    {
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+            linkedObject.InteractableObjectGrabbed -= InteractableObjectGrabbed;
+            linkedObject.InteractableObjectSnappedToDropZone -= InteractableObjectSnappedToDropZone;
+        }
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        if (objectToDisable != null)
+            objectToDisable.SetActive(false);
+    }
+
+    protected virtual void InteractableObjectGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        if (collider != null)
+            collider.isTrigger = true;
+    }
+
+    protected virtual void InteractableObjectSnappedToDropZone(object sender, InteractableObjectEventArgs e)
+    {
+        if (collider != null)
+            collider.isTrigger = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Food"))
+            objectToDisable = (objectToDisable == null ? other.gameObject : objectToDisable);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objectToDisable = null;
+
+    }
+}
